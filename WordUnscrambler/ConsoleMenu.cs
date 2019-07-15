69843430108e9ec1ab95eb509540f7752fedb800
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-07-15 12:17:51 -0400

    Menu now asks for menu option.

--- WordUnscrambler/ConsoleMenu.cs
+++ WordUnscrambler/ConsoleMenu.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,28 +10,63 @@ namespace WordUnscrambler
     class ConsoleMenu
     {
         public ConsoleColor menuOptionColor { get; set; } = ConsoleColor.White;
+        public int indent { get; set; } = 4;
 
 
         IDictionary<string, string> options = new Dictionary<string, string>();
 
-        public void setMenuOption(string key, string description)
+        // --------------------------------------------------------------------- PRINTING UTILITY FUNCTIONS
+        private void print(string message)
         {
-            options.Add(key, description);
+            string indentString = "";
+
+            for (int i = 0; i < indent; i++) indentString += " ";
+            Console.Write(indentString + message);
+        }
+        private void printLine(string message)
+        {
+            string indentString = "";
+
+            for (int i = 0; i < indent; i++) indentString += " ";
+            Console.WriteLine(indentString + message);
+        }
+        private string ToTitleCase(string str)
+        {
+            return new CultureInfo("en").TextInfo.ToTitleCase(str.ToLower());
         }
 
-        public void printMenuOption(string key)
+        // --------------------------------------------------------------------- MENU MANIPULATION
+        public void SetMenuOption(string key, string description)
+        {
+            options.Add(key.ToLower(), description);
+        }
+
+        // --------------------------------------------------------------------- MENU PRINTING
+        public void PrintMenuOption(string key)
         {
             Console.ForegroundColor = menuOptionColor;
-            Console.WriteLine(key);
+            print(ToTitleCase(key));
             Console.ResetColor();
-            Console.Write($" - {options[key]}");
+            Console.WriteLine($" - {options[key]}");
+        }
+        public void PrintMenu()
+        {
+            Console.WriteLine();
+            foreach (string key in options.Keys) PrintMenuOption(key);
+            Console.WriteLine();
         }
 
-        public void printMenu()
+        // --------------------------------------------------------------------- MENU INPUT
+        public string AskMenuOption()
         {
-            Console.WriteLine("--------------------------------------------------");
-            foreach (string key in options.Keys) printMenuOption(key);
-            Console.WriteLine("--------------------------------------------------");
+            print("Enter a menu option: ");
+            string option = Console.ReadLine();
+            while (!options.ContainsKey(option.ToLower()))
+            {
+                print("Please enter a valid option: ");
+                option = Console.ReadLine();
+            }
+            return option;
         }
     }
 }
--- WordUnscrambler/Program.cs
+++ WordUnscrambler/Program.cs
@@ -8,15 +8,19 @@ namespace WordUnscrambler
 {
     class Program
     {
+        static ConsoleMenu menu = new ConsoleMenu();
 
+        static void init()
+        {
+            menu.menuOptionColor = ConsoleColor.Green;
+            menu.SetMenuOption("F", "Submit words by file");
+            menu.SetMenuOption("M", "Submit words manually");
+        }
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            init();
+            menu.PrintMenu();
+            menu.AskMenuOption();
         }
     }
 }
